72c2fd6306fdc3f0fbf9615d950d20b257f31b03
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2024-05-13 16:43:57 +0700

    Update unet.py

--- models/unet.py
+++ models/unet.py
@@ -50,7 +50,7 @@ class UNet(nn.Module):
     def __init__(self, in_ch=3, out_ch=1):
         super(UNet, self).__init__()
 
-        n1 = 64
+        n1 = 32
         filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
         
         self.Maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
